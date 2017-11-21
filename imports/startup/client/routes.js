b921356a80cca22452bfc174c5268817bc34bd55
Paulos Liu <paulosliu@gmail.com>
2017-11-21 13:18:57 -0800

    finished porting, except travel page, cause it looks like garbage'

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -6,6 +6,8 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import '../../ui/layout/body/body.js';
 import '../../ui/pages/home/home.js';
 import '../../ui/pages/map/map.js'
+import '../../ui/pages/travel/travel.js'
+import '../../ui/pages/projects/projects.js'
 
 // components
 import '../../ui/components/navbar/navbar.js'
@@ -22,5 +24,19 @@ FlowRouter.route('/map', {
   name: 'App.map',
   action() {
     BlazeLayout.render('App_body', { main: 'App_map' });
+  },
+});
+
+FlowRouter.route('/travel', {
+  name: 'App.travel',
+  action() {
+    BlazeLayout.render('App_body', { main: 'App_travel' });
+  },
+});
+
+FlowRouter.route('/projects', {
+  name: 'App.projects',
+  action() {
+    BlazeLayout.render('App_body', { main: 'App_projects' });
   },
 });
