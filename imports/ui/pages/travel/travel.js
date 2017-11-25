f74e8b07dbd1e5ec883135532674ba337460ba7e
Paulos Liu <paulosliu@gmail.com>
2017-11-25 12:09:09 -0800

    made map.js code more maintainable

--- imports/ui/pages/travel/travel.js
+++ imports/ui/pages/travel/travel.js
@@ -1,1 +1,20 @@
 import './travel.html'
+import {ReactiveVar} from 'meteor/reactive-var';
+
+let p = [
+  {
+    title: "Title",
+    content: "hihihihihihhihihihihihihihihihihihihihihihihihihihihihihihihihihihihihii"
+  }
+];
+
+let projects = new ReactiveVar(p);
+
+Template.App_travel.helpers({
+  projects() {
+    if (projects) {
+      return projects.get();
+    }
+    return projects;
+  }
+});
